84ca0414d858ef8965a8b25054647555c455ae59
itworksinua <itworksinua@gmail.com>
2017-12-21 04:14:19 +0300

    init

--- WidgetManager/Controllers/HomeController.cs
+++ WidgetManager/Controllers/HomeController.cs
@@ -6,25 +6,41 @@ using System.Web.Mvc;
 
 namespace WidgetManager.Controllers
 {
+    #region Controller
     public class HomeController : Controller
     {
+        #region Crud
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Widget()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Title = "List Of Employees";
+            var positions = new List<string> { "Team Leader", "Project Manager", "Software Developer", "Senior Engineer", "Architect", "Network Administrator" };
 
-            return View();
+            var response = new List<Employe>();
+            for (int i = 0; i <= 100; i++)
+            {
+                response.Add(new Employe { Id = i, Name = "UserName " + i, LastName = "LastName " + i, Position = positions.OrderBy(x => Guid.NewGuid()).FirstOrDefault() });
+            }
+
+            return View(response as List<Employe>);
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        #endregion
     }
+    #endregion
+
+    #region Helper Classes
+    public class Employe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+    }
+    #endregion
 }
---
+++ WidgetManager/Scripts/widget.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $("#getVidgetBtn").click(function () {
+        $("#employees_list").remove();
+        var url = window.location.origin.toString() + "/Home/Widget";
+        var jqxhr = $.ajax(url)
+            .done(function (response) {
+                $("body").append(response);
+            }).fail(function () {
+            }).always(function () { });
+    });
+});
